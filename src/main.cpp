6398766a62b8f7f1f6f526fd070a8afa47e619f9
Justin Lee <justin.lee007@gmail.com>
2017-04-28 14:15:55 -0700

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Eigen/Dense"
+#include <vector>
+#include "ukf.h"
+
+using namespace std;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using std::vector;
+
+int main() {
+
+  // Create a UKF instance
+  UKF ukf;
+
+  /*******************************************************************************
+  * Programming assignment calls
+  *******************************************************************************/
+
+  MatrixXd Xsig = MatrixXd(11, 5);
+  ukf.GenerateSigmaPoints(&Xsig);
+
+  MatrixXd Xsig_aug = MatrixXd(15, 7);
+  ukf.AugmentedSigmaPoints(&Xsig_aug);
+
+  MatrixXd Xsig_pred = MatrixXd(15, 5);
+  ukf.SigmaPointPrediction(&Xsig_pred);
+
+  return 0;
+}
