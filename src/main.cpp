b8643f5a0f90412ce76d10057cb374971a8a9e09
Justin Lee <justin.lee007@gmail.com>
2017-04-28 15:13:38 -0700

    Added predict radar measurement

--- src/main.cpp
+++ src/main.cpp
@@ -30,5 +30,9 @@ int main() {
   MatrixXd P_pred = MatrixXd(5, 5);
   ukf.PredictMeanAndCovariance(&x_pred, &P_pred);
 
+  VectorXd z_out = VectorXd(3);
+  MatrixXd S_out = MatrixXd(3, 3);
+  ukf.PredictRadarMeasurement(&z_out, &S_out);
+
   return 0;
 }
